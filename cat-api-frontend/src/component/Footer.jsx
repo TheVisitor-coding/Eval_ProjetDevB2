78687af758013e0451ea7a0bd471b5ac9afa399c
MatteoRossi_Dev <designbymatrossi@gmail.com>
2023-03-26 22:19:23 +0200

    finish

--- cat-api-frontend/src/component/Footer.jsx
+++ cat-api-frontend/src/component/Footer.jsx
@@ -13,22 +13,42 @@ function Footer ({cat}) {
     <p>Poids en Imperial : {cat.weight.imperial}</p>
   </div>
 
-      {/* <table className="footer">
-        <thead>
-          <th>Informations un peu cool</th>
-          <th>Ses compétences de classe</th>
-        </thead>
+  <div class="competences">
+    <h3>Ses Compétences de classe</h3>
+    <table>
+      <tr>
+        <td>Adaptabilité :</td>
+        <td>Niveau {cat.adaptability}</td>
+      </tr>
+      <tr>
+        <td>Affection :</td>
+        <td>Niveau {cat.affection_level}</td>
+      </tr>
+      <tr>
+        <td>Energie :</td>
+        <td>Niveau {cat.energy_level}</td>
+      </tr>
+      <tr>
+        <td>Intelligence :</td>
+        <td>Niveau {cat.intelligence}</td>
+      </tr>
+      <tr>
+        <td>Besoin Social :</td>
+        <td>Niveau {cat.social_needs}</td>
+      </tr>
+      <tr>
+        <td>Rareté :</td>
+        <td>Niveau {cat.rare}</td>
+      </tr>
+      <tr>
+        <td>Vie intérieur :</td>
+        <td>Niveau {cat.indoor}</td>
+      </tr>
+    </table>
+  </div>
+</div>
 
-        <tbody>
-          <tr>
-            <td>Son tempérament : {cat.temperament}</td>
-            <td>Sa durée de vie : {cat.life_span}</td>
-            </tr>
-          <tr>
-            <td>{cat.adaptability} en adaptabilité</td>
-          </tr>
-        </tbody>
-        </table> */}
+
     </>
    );
 }
